309eaefe4549dd83815f5783970c347bb42b5c66
justusjonas74 <francis.doege@web.de>
2018-03-26 13:21:14 +0000

    Refactorising und bugfixes

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const mongoose = require('mongoose');
 // TODO LIST
 // Create Index StopTime. stop_id, departure_time, arrival_time, stop_name
 
-// CONNECT DATABASE
+// CONNECT THE DATABASE
 const config = require('./config.json');
 mongoose.connect(config.mongoUrl)
 
-
 // START THE SERVER
 require('./lib/server').server(app)
-
