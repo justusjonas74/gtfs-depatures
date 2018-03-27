d088c81069245f63202633ee433e9dbcf3f2ac66
justusjonas74 <francis.doege@web.de>
2018-03-27 05:35:27 +0000

    standard --fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//DEPENDENCIES
+// DEPENDENCIES
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // TODO LIST
 // Create Index StopTime.stop_id, StopTime.departure_time, StopTime.arrival_time, Stop.stop_name, Stop.parrent_station
 
 // CONNECT THE DATABASE
-const config = require('./config.json');
+const config = require('./config.json')
 mongoose.connect(config.mongoUrl)
 
 // START THE SERVER
