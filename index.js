6562de4095dcca6ab6bb564f4e23decf3df6f5cc
justusjonas74 <francis.doege@web.de>
2018-03-27 12:23:47 +0000

    Added support for separated test,dev and production databases

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const mongoose = require('mongoose')
 
 // CONNECT THE DATABASE
 const config = require('./config.json')
-mongoose.connect(config.mongoUrl)
+switch (process.env.NODE_ENV) {
+  case 'test':
+    mongoose.connect(config.mongoUrl.test)
+    break
+  case 'production':
+    mongoose.connect(config.mongoUrl.production)
+    break
+  default: // include development
+    mongoose.connect(config.mongoUrl.development)
+}
 
 // START THE SERVER
 require('./lib/server').server(app)
