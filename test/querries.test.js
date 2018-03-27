180a9c77f4701720864590683e189f577b0f63e2
justusjonas74 <francis.doege@web.de>
2018-03-27 05:34:45 +0000

    Setup test environment

---
+++ test/querries.test.js
@@ -0,0 +1,30 @@
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test'
+
+// let mongoose = require('mongoose')
+
+// let { getDeparturesByStops, getStopByID, getStopByName, getStopArrayByID } = require('../lib/querries')
+
+// Require the dev-dependencies
+let chai = require('chai')
+// let chaiHttp = require('chai-http')
+// let server = require('../server');
+chai.should()
+
+// chai.use(chaiHttp);
+
+describe('querries.getDeparturesByStops', () => {
+
+})
+
+describe('querries.getStopByID', () => {
+
+})
+
+describe('querries.getStopByName', () => {
+
+})
+
+describe('querries.getStopArrayByID', () => {
+
+})
