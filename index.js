fb2ecdf142a14eee9fa02b63d81a6366545587d3
justusjonas74 <francis.doege@web.de>
2018-03-28 09:42:40 +0000

    Excluded config from db.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const mongoose = require('mongoose')
 
 // CONNECT THE DATABASE
-require('./lib/db').connectDatabase(mongoose)
+const config = require('../config.json')
+require('./lib/db').connectDatabase(mongoose, config)
 
 // START THE SERVER
 require('./lib/server').server(app)
@@ -13,3 +14,6 @@ require('./lib/server').server(app)
 // Create Index StopTime.stop_id, StopTime.departure_time, StopTime.arrival_time, Stop.stop_name, Stop.parrent_station
 // Create a wildcard route for all unsupported requests
 // Create a version for the api
+// Create a new importer
+// FIX OFFSET RANGE > 23:59:59
+// HANDLE DEPARTURES  > 23:59:59
