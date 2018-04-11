6a3f155820c157a2539a69ad33a2a848d8ec2bd0
justusjonas74 <francis.doege@web.de>
2018-04-11 12:28:19 +0000

    Separated environement specific functions

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 // DEPENDENCIES
-// const express = require('express')
-// const app = express()
 const mongoose = require('mongoose')
 
-// CONNECT THE DATABASE
+// LOCAL DEPENDENCIES
+const getEnvMongoUrl = require('./lib/getEnvMongoUrl')
 const config = require('./config.json')
-require('./lib/db').connectDatabase(mongoose, config)
 
-// SERVER
+// CONNECT THE DATABASE
+const mongoUrl = getEnvMongoUrl(config)
+mongoose.connect(mongoUrl)
+
+// LOAD AND CONFIG SERVER
 const app = require('./lib/server')
 
 // START THE SERVER
 const port = process.env.PORT || 5000
 app.listen(port).on('error', function (err) { console.log(err) })
 console.log(`App listening on port: ${port}`)
-
-// TODO LIST
-// Check how to test error handling. It's not possible for me to produce an err
-// Exclude test data from test files (BE D-R-Y NOT W-E-T)
-// Add Option "includeChilds" to all route
-// Add Option "time" and "offset" to the route ../../departures
-// Create Index StopTime.stop_id, StopTime.departure_time, StopTime.arrival_time, Stop.stop_name, Stop.parrent_station
-// Create a wildcard route for all unsupported requests
-// Create a version for the api
-// Create a new importer
-// FIX OFFSET RANGE > 23:59:59
-// HANDLE DEPARTURES  > 23:59:59
